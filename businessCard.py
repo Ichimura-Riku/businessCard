198bab0a647bb814ec3ea9f568d70d1bceb87afb
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-05-14 09:40:18 +0900

    Trueに戻した

--- businessCard.py
+++ businessCard.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 image_ichimura = Image.open('businessCard_ichimura.png')
 
-st.image(image_ichimura, use_column_width=False)
+st.image(image_ichimura, use_column_width=True)
 
 image_katsuki = Image.open('businessCard_katsuki.png')
 
-st.image(image_katsuki, use_column_width=False)
+st.image(image_katsuki, use_column_width=True)
